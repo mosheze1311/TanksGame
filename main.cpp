53fc19fc4c2bebd939f57598c51b5e12e59585c2
mosheze1311 <mosheze1311@gmail.com>
2025-04-23 11:40:21 +0300

    adjust to changes

--- main.cpp
+++ main.cpp
@@ -6,8 +6,8 @@
 
 int main(int argc, char *argv[])
 {
-    if (argc != 2){
-        Logger::runtime().logError(std::format("Arguments do not match to programm - expecting 1, got {}", argc-1));
+    if (argc != 2 && argc != 3){
+        Logger::runtime().logError(std::format("Arguments do not match to programm - expecting 1 (or 2 for drawing), got {}", argc-1));
         return 0;
     }
     const string file_path = argv[1];
@@ -16,12 +16,21 @@ int main(int argc, char *argv[])
         Logger::runtime().logError(std::format("Can't create board"));
         return 0;
     }
-    
-    
-    Player p1(b->getTanks(b->getGameObjects(GameObjectType::tank1)));
-    Player p2(b->getTanks(b->getGameObjects(GameObjectType::tank2)));
-
-    GameManager gm(*b, p1, p2,(string) "");
-    gm.play();
+    DrawingType dt = DrawingType::NONE;
+    if(argc == 3) // if drawing is requested
+    {
+        try
+        {
+            dt = DrawingTypes::fromInt(std::stoi(argv[2]));
+        }
+        catch (...)
+        {
+            Logger::runtime().logError(std::format("If given, 3rd argument must be a number"));
+            return 0;
+        }
+    }
+    Player p1(GameObjectType::tank1), p2(GameObjectType::tank2);
+    GameManager gm(*b, p1, p2);
+    gm.play(dt);
     return 0;
 }
