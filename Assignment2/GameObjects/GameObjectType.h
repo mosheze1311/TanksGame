090662183c5542a8274a2db11325f8e2fb2777ca
mosheze1311 <mosheze1311@gmail.com>
2025-05-13 14:01:20 +0300

    add utils function

--- Assignment2/GameObjects/GameObjectType.h
+++ Assignment2/GameObjects/GameObjectType.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <array>
 
 enum class GameObjectType
 {
@@ -7,4 +8,25 @@ enum class GameObjectType
     WALL = '#',
     MINE = '@',
     SHELL = '*'
+};
+
+namespace GameObjectTypeUtils
+{
+    constexpr std::array<GameObjectType, 5> valid_types = {
+        GameObjectType::TANK1,
+        GameObjectType::TANK2,
+        GameObjectType::WALL,
+        GameObjectType::MINE,
+        GameObjectType::SHELL
+    };
+
+    inline bool isValidObjectChar(char obj_char)
+    {
+        for (GameObjectType type : GameObjectTypeUtils::valid_types)
+        {
+            if (obj_char == static_cast<char>(type))
+                return true;
+        }
+        return false;
+    }
 };
