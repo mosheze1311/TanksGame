e4209de9a476874f6c5d98bf938e008430931b44
mosheze1311 <mosheze1311@gmail.com>
2025-05-18 16:00:43 +0300

    adjust for 9 players

--- Assignment2/GameObjects/GameObjectType.h
+++ Assignment2/GameObjects/GameObjectType.h
@@ -5,16 +5,32 @@ enum class GameObjectType
 {
     TANK1 = '1',
     TANK2 = '2',
+    TANK3 = '3',
+    TANK4 = '4',
+    TANK5 = '5',
+    TANK6 = '6',
+    TANK7 = '7',
+    TANK8 = '8',
+    TANK9 = '9',
     WALL = '#',
     MINE = '@',
-    SHELL = '*'
+    SHELL = '*',
+
 };
 
 namespace GameObjectTypeUtils
 {
-    constexpr std::array<GameObjectType, 5> valid_types = {
+    constexpr std::array<GameObjectType, 12> valid_types = {
         GameObjectType::TANK1,
         GameObjectType::TANK2,
+        GameObjectType::TANK3,
+        GameObjectType::TANK4,
+        GameObjectType::TANK5,
+        GameObjectType::TANK6,
+        GameObjectType::TANK7,
+        GameObjectType::TANK8,
+        GameObjectType::TANK9,
+
         GameObjectType::WALL,
         GameObjectType::MINE,
         GameObjectType::SHELL
@@ -28,5 +44,12 @@ namespace GameObjectTypeUtils
                 return true;
         }
         return false;
+    }
+
+    inline int tankTypeToPlayerIndex(GameObjectType type)
+    {
+        char playerChar = static_cast<char>(type);  
+        int playerId = playerChar - '0';      
+        return playerId;  
     }
 };
