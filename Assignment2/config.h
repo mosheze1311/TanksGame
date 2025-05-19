a8565290433e185c84fb5d9c20cba8d60cdc9ab6
mosheze1311 <mosheze1311@gmail.com>
2025-05-19 15:38:51 +0300

    temporary config file

---
+++ Assignment2/config.h
@@ -0,0 +1,28 @@
+#pragma once
+
+// SPEED
+#define shell_speed 2
+
+// SPAWN DIRECTION
+#define tank1_initial_direction Direction::LEFT
+#define tank2_initial_direction Direction::RIGHT
+#define tank3_initial_direction Direction::RIGHT
+#define tank4_initial_direction Direction::RIGHT
+#define tank5_initial_direction Direction::RIGHT
+#define tank6_initial_direction Direction::RIGHT
+#define tank7_initial_direction Direction::RIGHT
+#define tank8_initial_direction Direction::RIGHT
+#define tank9_initial_direction Direction::RIGHT
+
+// HP
+#define wall_hp 1
+#define tank_hp 1
+
+// DAMAGE
+#define shell_damage 1
+#define mine_damage 1
+#define collision_damage 1
+
+
+
+
