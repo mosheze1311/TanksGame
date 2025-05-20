559e2e6351a145135b45fc13df7be0e233d91841
itaim900 <itaim900@gmail.com>
2025-05-20 12:45:35 +0300

    dijkstra fixed, works with analyical sat view
    Co-authored-by: Moshe Zeitouny <mosheze1311@users.noreply.github.com>

--- Assignment2/TankAlgorithm/AbstractTankAlgorithm.h
+++ Assignment2/TankAlgorithm/AbstractTankAlgorithm.h
@@ -3,6 +3,7 @@
 
 #include "../GameCollisionHandler/GameCollisionHandler.h"
 #include "../SatelliteView/BoardSatelliteView.h"
+#include "../SatelliteAnalyticsView/SatelliteAnalyticsView.h"
 
 #include <queue>
 #include <set>
@@ -11,51 +12,57 @@ class AbstractTankAlgorithm : public TankAlgorithm
 {
 
 private:
-    void Dijkstra(const  SatelliteView& sat_view, GameObjectType tank_type, BoardCell start, BoardCell target, map<BoardCell, int> &distances, map<BoardCell, BoardCell> &parents)
+
+    void Dijkstra(const SatelliteAnalyitcsView& sat_view, GameObjectType tank_type, BoardCell start, BoardCell target, map<BoardCell, int> &distances, map<BoardCell, BoardCell> &parents)
     {
-        // priority_queue<
-        //     pair<int, pair<BoardCell, BoardCell>>,
-        //     vector<pair<int, pair<BoardCell, BoardCell>>>,
-        //     greater<pair<int, pair<BoardCell, BoardCell>>>>
-        //     q;
-        // q.push({0, {start, start}});
-        // set<BoardCell> visited;
+        priority_queue<
+            pair<int, pair<BoardCell, BoardCell>>,
+            vector<pair<int, pair<BoardCell, BoardCell>>>,
+            greater<pair<int, pair<BoardCell, BoardCell>>>>
+            q;
+        q.push({0, {start, start}});
+        set<BoardCell> visited;
 
-        // while (!q.empty())
-        // {
-        //     // access and pop first item in heap
-        //     auto [dist, current_and_parent_pair] = q.top();
-        //     auto [c, parent] = current_and_parent_pair;
-        //     q.pop();
+        while (!q.empty())
+        {
+            // access and pop first item in heap
+            auto [dist, current_and_parent_pair] = q.top();
+            auto [c, parent] = current_and_parent_pair;
+            q.pop();
 
-        //     // skip visited since inserting cells multiple times
-        //     if (visited.find(c) != visited.end())
-        //     {
-        //         continue;
-        //     }
+            // skip visited since inserting cells multiple times
+            if (visited.find(c) != visited.end())
+            {
+                continue;
+            }
 
-        //     // mark visted and save distance, parent data
-        //     visited.insert(c);
-        //     parents[c] = parent;
-        //     distances[c] = dist;
+            // mark visted and save distance, parent data
+            visited.insert(c);
+            parents[c] = parent;
+            distances[c] = dist;
 
-        //     // early termination when reaching target
-        //     if (c == target)
-        //         break;
+            // early termination when reaching target
+            if (c == target)
+                break;
 
-        //     // adding neighbors to heap - only if tank can safely step there
-        //     for (BoardCell neighbor : GameBoardUtils::getAdjacentCells(c, , ))
-        //     {
-        //         if (target == neighbor || GameCollisionHandler::canObjectSafelyStepOn(board, tank_type, neighbor))
-        //         {
-        //             q.push({dist + 1, {neighbor, c}});
-        //         }
-        //     }
-        // }
+            // adding neighbors to heap - only if tank can safely step there
+            for (BoardCell neighbor : GameBoardUtils::getAdjacentCells(c, sat_view.getWidth(), sat_view.getHeight()))
+            {
+                if (visited.find(neighbor) != visited.end())
+                {
+                    continue;
+                }
+
+                if (neighbor == target || GameCollisionHandler::canObjectSafelyStepOn(sat_view, tank_type, neighbor))
+                {
+                    q.push({dist + 1, {neighbor, c}});
+                }
+            }
+        }
     }
 
-// TankAction Player::getTankEvasionAction(const GameBoard &board, const Tank *tank, const MovableObject *evade_from) const
-// {
+    ActionRequest getTankEvasionAction(const  SatelliteView& sat_view, const Tank *tank, const MovableObject *evade_from) const
+ {
 //     // try to escape from tank/bullet
 
 //     // --- bullet escape ---
@@ -98,7 +105,36 @@ private:
 //     // otherwise accept your faith
 
 //     return TankAction::NOTHING; // should not get here - only if evade_from not on board.
-// }
+ }
 
+
+ActionRequest getTankAggressiveAction(const  SatelliteView& sat_view, const Tank *tank) const
+{
+    // // try to chase the enemy tank or shoot at it.
+
+    // // dijkstra to closest tank
+    // // if can shoot it - shoot
+    // // if can't shoot it - chase
+    // optional<BoardCell> opt = board.getObjectLocation(tank);
+    // if (!opt)
+    // {
+    //     return TankAction::NOTHING; // should not happen
+    // }
+
+    // BoardCell start = opt.value();
+    // BoardCell target = this->approxClosestEnemyTankLocation(board, start);
+
+    // // try to shoot enemy
+    // if(auto shoot_action_opt = attemptShoot(board, tank, start, target)){
+    //     return shoot_action_opt.value();
+    // }
+    
+    // // if will be able to shoot in the future, wait 
+    // if (PlayerUtils::inShootRange(board, start, target) && board.isStraightLine(start, target) && tank->getShells() > 0)
+    // {
+    //     return TankAction::NOTHING;
+    // }
+    // return advanceTankToTarget(board, tank, start, target);
+}
 
 };
