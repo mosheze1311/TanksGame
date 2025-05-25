d6fc3cffd917f204a352543e097a4433ea4816d1
mosheze1311 <mosheze1311@gmail.com>
2025-05-25 12:33:51 +0300

    Added TODO:

--- Assignment2/TankAlgorithmFactory/MyTankAlgorithmFactory.h
+++ Assignment2/TankAlgorithmFactory/MyTankAlgorithmFactory.h
@@ -8,18 +8,18 @@
 using std::unique_ptr;
 
 // TODO: finish implementation
-class MyTankAlgorithmFactory : public TankAlgorithmFactory{
-    public:
+class MyTankAlgorithmFactory : public TankAlgorithmFactory
+{
+public:
+    // === Destructor === //
+    ~MyTankAlgorithmFactory() override = default;
 
-        // === Destructor === //
-        ~MyTankAlgorithmFactory() override = default;
+    // === Producing TankAlgorithm objects === //
+    unique_ptr<TankAlgorithm> create(int player_index, int tank_index) const override
+    {
+        if (player_index % 2 == 1)
+            return std::make_unique<AggressiveTankAlgorithm>(player_index, tank_index);
 
-        // === Producing TankAlgorithm objects === //
-        unique_ptr<TankAlgorithm> create(int player_index, int tank_index) const override
-        {
-            if (player_index % 2 == 1)
-                return std::make_unique<AggressiveTankAlgorithm>(player_index, tank_index);
-
-            return std::make_unique<PassiveTankAlgorithm>(player_index, tank_index);
-        }
+        return std::make_unique<PassiveTankAlgorithm>(player_index, tank_index);
+    }
 };
