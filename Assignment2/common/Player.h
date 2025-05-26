826ef97fc3d00ad062e6cca67e298aca969cd79a
mosheze1311 <mosheze1311@gmail.com>
2025-05-26 11:43:59 +0300

    pedantic flag patch

--- Assignment2/common/Player.h
+++ Assignment2/common/Player.h
@@ -8,7 +8,13 @@ class Player
 public:
     Player(int player_index,
            size_t x, size_t y,
-           size_t max_steps, size_t num_shells) {}
+           size_t max_steps, size_t num_shells) {
+        (void)player_index;
+        (void)x;
+        (void)y;
+        (void)max_steps;
+        (void)num_shells;
+           }
     virtual ~Player() {}
     virtual void updateTankWithBattleInfo(TankAlgorithm &tank, SatelliteView &satellite_view) = 0;
 };
