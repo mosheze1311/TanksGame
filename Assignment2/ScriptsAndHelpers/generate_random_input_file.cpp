f576caf45ad9ac84424528e9ae10cbaa8e3f7c85
mosheze1311 <mosheze1311@gmail.com>
2025-06-24 20:05:52 +0300

    fix random input generator

--- Assignment2/ScriptsAndHelpers/generate_random_input_file.cpp
+++ Assignment2/ScriptsAndHelpers/generate_random_input_file.cpp
@@ -9,6 +9,13 @@ enum class GameObjectType
 {
     TANK1 = '1',
     TANK2 = '2',
+    TANK3 = '3',
+    TANK4 = '4',
+    TANK5 = '5',
+    TANK6 = '6',
+    TANK7 = '7',
+    TANK8 = '8',
+    TANK9 = '9',
     WALL = '#',
     MINE = '@'
 };
