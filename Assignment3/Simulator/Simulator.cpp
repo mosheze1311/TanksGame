ffa0543e95b23c7f18a1a63a0192c801655f54bb
mosheze1311 <mosheze1311@gmail.com>
2025-06-26 17:08:20 +0300

    added methods for creating the output files.
    added runManagers...() methods for testing before threads utilization.

--- Assignment3/Simulator/Simulator.cpp
+++ Assignment3/Simulator/Simulator.cpp
@@ -1,8 +1,12 @@
 #include "Simulator.h"
 #include "Registrar/Registrar.h"
 
+#include "../UserCommon/Utils/Timestamp.h"
+#include "../UserCommon/Utils/GameResultUtils.h"
+#include "../UserCommon/Utils/SatelliteViewUtils.h"
+
 #include <dlfcn.h>
-#include <iostream>
+#include <iostream> // TODO: delete this include
 
 using namespace UserCommon_211388913_322330820;
 // === Private Functions === //
@@ -94,6 +98,57 @@ void Simulator::runComparativeMode()
     loadAlgorithmFile(comparative_args.getAlgorithm2());
 
     // TODO: run all with threads
+    this->runManagersOnComparativeMode();
+
+    // create output file
+    createComparativeOutput();
+}
+void Simulator::runManagersOnComparativeMode()
+{
+    GameResult a{};
+    BoardSatelliteView a_view = this->maps_details[0].getSatelliteView();
+    a.winner = 1;
+    a.reason = GameResult::ALL_TANKS_DEAD;
+    a.remaining_tanks = {1, 0};
+    a.gameState = std::make_unique<BoardSatelliteView>(a_view);
+    GameResult b{};
+    BoardSatelliteView b_view = this->maps_details[0].getSatelliteView();
+    b.winner = 1;
+    b.reason = GameResult::ALL_TANKS_DEAD;
+    b.gameState = std::make_unique<BoardSatelliteView>(b_view);
+    b.remaining_tanks = {1, 0};
+    GameResult c{};
+    BoardSatelliteView c_view = this->maps_details[0].getSatelliteView();
+    c.winner = 2;
+    c.reason = GameResult::ALL_TANKS_DEAD;
+    c.gameState = std::make_unique<BoardSatelliteView>(c_view);
+    c.remaining_tanks = {0, 1};
+    GameResult d{};
+    BoardSatelliteView d_view = this->maps_details[0].getSatelliteView();
+    d.winner = 0;
+    d.reason = GameResult::ALL_TANKS_DEAD;
+    d.gameState = std::make_unique<BoardSatelliteView>(d_view);
+    d.remaining_tanks = {0, 0};
+
+    for (auto manager_factory_entry : Registrar<GameManagerFactory>::getRegistrar())
+    {
+        auto manager = manager_factory_entry.getFactory()(true);
+        auto player_ptr = Registrar<PlayerFactory>::getRegistrar()[0].getFactory()(1, 1ul, 1ul, 1ul, 1ul);
+        auto algoFactory = Registrar<TankAlgorithmFactory>::getRegistrar()[0].getFactory();
+        auto sat_view = this->maps_details[0].getSatelliteView();
+                            GameResult e = manager->run(this->maps_details[0].getWidth(), this->maps_details[0].getHeight(),
+                                                        sat_view,
+                                                        this->maps_details[0].getMaxSteps(), this->maps_details[0].getNumShells(),
+                                                        *player_ptr, *player_ptr, algoFactory, algoFactory);
+        comparative_results["fifth"] = std::move(e);
+
+        break;
+    }
+
+    comparative_results["first"] = std::move(a);
+    comparative_results["third"] = std::move(c);
+    comparative_results["second"] = std::move(b);
+    comparative_results["fourth"] = std::move(d);
 }
 
 void Simulator::runCompetitiveMode()
@@ -120,14 +175,129 @@ void Simulator::runCompetitiveMode()
         }
     }
 
-    // TODO: run all with threads
+    // TODO: run all with threads and update scores
+
+    // create output file
+    createCompetitiveOutput();
+}
+void Simulator::runManagersOnCompetitiveMode()
+{
+    competitive_results["algo1"] = 10;
+    competitive_results["algo2"] = 40;
+    competitive_results["algo3"] = 15;
+    competitive_results["algo4"] = 12;
+    competitive_results["algo5"] = 0;
+    competitive_results["algo6"] = 13;
+}
+
+// === Output Files === //
+std::string Simulator::getComparativeOutputFileName() const
+{
+    return "comparative_results_" + getTimestampForNow() + ".txt";
+}
+std::string Simulator::getCompetitiveOutputFileName() const
+{
+    return "competition_" + getTimestampForNow() + ".txt";
+}
+std::vector<std::pair<std::string, std::vector<std::string>>> Simulator::createSortedGroupedManagers(const std::map<std::string, GameResult> &managers_results) const
+{
+    /*
+    This is a helper function for the createComparativeOutput() method
+    */
+
+    using namespace UserCommon_211388913_322330820::GameResultUtils; // use the namespace to get the GameResult == operator
+
+    // first group the managers with equal result objects
+    std::vector<std::pair<std::string, std::vector<std::string>>> sorted_grouped_managers;
+    for (auto &[manager, result] : managers_results)
+    {
+        bool group_exists = false;
+        for (auto &[first_manager, equal_managers] : sorted_grouped_managers)
+        {
+            if (result == managers_results.at(first_manager))
+            {
+                group_exists = true;
+                equal_managers.push_back(manager);
+                break;
+            }
+        }
+        if (!group_exists)
+        {
+            sorted_grouped_managers.emplace_back(manager, std::vector<std::string>({manager}));
+        }
+    }
+
+    // sort the vector in decsending order by amount of managers that share the same reesult (size of the inner vector)
+    std::sort(sorted_grouped_managers.begin(), sorted_grouped_managers.end(),
+              [](const auto &a, const auto &b)
+              {
+                  return a.second.size() > b.second.size();
+              });
+
+    return sorted_grouped_managers;
+}
+void Simulator::logManagerGroup(const std::string &representative_manager, const std::vector<std::string> &group,
+                                const std::string &comparative_output_file) const
+{
+    for (size_t i = 0; i < group.size(); ++i)
+    {
+        std::string manager = group[i];
+        std::string separator = i < group.size() - 1 ? ", " : "\n";
+        Logger::output(comparative_output_file).log(manager + separator);
+    }
+
+    const GameResult &result = comparative_results.at(representative_manager);
+    Logger::output(comparative_output_file).log(GameResultUtils::toString(result));
+}
+
+void Simulator::createComparativeOutput() const
+{
+    auto &comparative_args = dynamic_cast<ParsedComparativeCmdArguments &>(*args);
+    std::string comparative_output_file = getComparativeOutputFileName();
+
+    Logger::output(comparative_output_file).logLine("game_map=" + comparative_args.getGameMap());
+    Logger::output(comparative_output_file).logLine("algorithm1=" + comparative_args.getAlgorithm1());
+    Logger::output(comparative_output_file).logLine("algorithm2=" + comparative_args.getAlgorithm2());
+    Logger::output(comparative_output_file).logLine("");
+
+    std::vector<std::pair<std::string, std::vector<std::string>>> sorted_grouped_managers = createSortedGroupedManagers(comparative_results);
+    //  print managers result groups
+    for (size_t i = 0; i < sorted_grouped_managers.size(); ++i)
+    {
+        auto &[representative_manager, group] = sorted_grouped_managers[i];
+        logManagerGroup(representative_manager, group, comparative_output_file);
+
+        if (i != sorted_grouped_managers.size() - 1)
+            Logger::output(comparative_output_file).logLine("");
+    }
+}
+
+void Simulator::createCompetitiveOutput() const
+{
+    auto &competitive_args = dynamic_cast<ParsedCompetitiveCmdArguments &>(*args);
+    std::string competition_output_file = getCompetitiveOutputFileName();
+
+    Logger::output(competition_output_file).logLine("game_maps_folder=" + competitive_args.getGameMapsFolder());
+    Logger::output(competition_output_file).logLine("game_manager=" + competitive_args.getGameManager());
+    Logger::output(competition_output_file).logLine("");
+
+    std::vector<std::pair<std::string, size_t>> sorted_algorithms(competitive_results.begin(), competitive_results.end());
+    std::sort(sorted_algorithms.begin(), sorted_algorithms.end(), [](const auto &a, const auto &b)
+              {
+                  return a.second > b.second; // descending order by score
+              });
+    for (const auto &entry : sorted_algorithms)
+    {
+        Logger::output(competition_output_file).logLine(entry.first + " " + std::to_string(entry.second));
+    }
 }
 
 // === Public Functions == //
-
 // === Consturctor === //
 Simulator::Simulator(std::unique_ptr<ParsedCmdArguments> args)
-    : args(std::move(args)) {}
+    : args(std::move(args))
+{
+}
 
 // === Public API === //
 void Simulator::run()
--- Assignment3/Simulator/Simulator.h
+++ Assignment3/Simulator/Simulator.h
@@ -2,6 +2,9 @@
 
 #include "ParsedCmdArguments.h"
 #include "MapFileParser/MapFileParser.h"
+
+#include "../common/GameResult.h"
+
 #include "../UserCommon/BoardSatelliteView/BoardSatelliteView.h"
 #include "../UserCommon/Logger/Logger.h"
 
@@ -16,11 +19,13 @@ private:
     // === Attributes === //
     std::unique_ptr<ParsedCmdArguments> args;
     std::vector<MapFileParser> maps_details;
+    std::map<std::string, GameResult> comparative_results;
+    std::map<std::string, size_t> competitive_results;
 
     // === Loading SO Files === //
     void loadAlgorithmFile(const std::string &algorithm_file);
     void loadManagerFile(const std::string &manager_file);
-    void loadSOFile(const std::string& file_path);
+    void loadSOFile(const std::string &file_path);
 
     // === Read Maps === //
     void readMapFile(const std::string &game_map_file);
@@ -28,9 +33,19 @@ private:
     // === Run Modes === //
     void runComparativeMode();
     void runCompetitiveMode();
+    void runManagersOnComparativeMode();
+    void runManagersOnCompetitiveMode();
 
-    // === Logging Functions === //
+    // === Output Files === //
+    std::string getComparativeOutputFileName() const;
+    void createComparativeOutput() const;
+    std::vector<std::pair<std::string, std::vector<std::string>>> createSortedGroupedManagers(const std::map<std::string, GameResult> &managers_results) const;
     
+    std::string getCompetitiveOutputFileName() const;
+    void createCompetitiveOutput() const;
+
+    void logManagerGroup(const std::string &representative_manager, const std::vector<std::string> &group,
+                        const std::string &comparative_output_file) const;
 
 public:
     // === Constructor === //
